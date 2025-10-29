44c2e32d096832edbc8415485a7f54dea3f8e8c7
Kenneth Burchfiel <kburchfiel@gmail.com>
2025-10-28 23:51:34 -0400

    Made a number of improvements, including a more streamlined display and color coding

--- cpp_world_clock.cpp
+++ cpp_world_clock.cpp
@@ -1,12 +1,31 @@
-/* C++ World Clock: 2025 Edition
+/* C++ World Clock: 2025 Edition [Still a work in progress!]
 By Ken Burchfiel
-Released under the MIT License */
+Released under the MIT License 
+
+// To-dos:
+1. Allow users to specify time zones via a config file that the
+script will read in
+3. Allow users to choose whether or not to show the Unix time entry
+
+(For your config file, you could place the time zone list at the 
+bottom and all other entries before it. Each entry could be preceded
+by an explanation, and you could hard-code the row numbers
+in your script for easier parsing. For instance, row 1 could be 
+"Show unix time", row 2 could be "true" or "false", 
+rows 3 and 4 could be an explanation/entry pair for some other
+configuration, row 5 could be "Time Zones", and rows 6 onward could
+be all the time zones. (The script would then loop from 5 to the end
+of all lines within the file to gather users' time zones.)
+
+*/
+
 
 #include <chrono>
 #include <format>
 #include <iostream>
 #include <vector>
 #include <thread>
+#include <string>
 
 // Initializing a list of time zone strings:
 // These can be found at:
@@ -27,43 +46,80 @@ std::vector<std::string> tz_string_list{
     "Australia/Sydney",
     "Pacific/Kiritimati",};
 
-// Note: I wasn't able to use
+
 std::string get_tz_time(
     std::string tz_string,
-    std::chrono::time_point<std::chrono::system_clock> current_time) {
+    std::chrono::time_point<std::chrono::system_clock, 
+    std::chrono::seconds> current_time) {
   // Note: I found the actual type of system_clock::now() from
   // https://en.cppreference.com/w/cpp/chrono/system_clock/now .
 
   // The following code was based on
   // https://en.cppreference.com/w/cpp/chrono/zoned_time.html .
 
+  
   std::chrono::zoned_time tz_time{tz_string, current_time};
+  
 
   // The following line was based on
   // https://stackoverflow.com/a/68754043/13097194
   // and https://en.cppreference.com/w/cpp/chrono/zoned_time/formatter.html .
-  return std::format("{:%FT%T%z}", tz_time);
+  // ISO8601 approach: (Probably overkill for this use case, though)
+  // return std::format("{:%FT%T%z}", tz_time);
+  return std::format("{:%T (%F) (%z)}", tz_time);
+
+
   //return tz_time;
 }
 
 void print_tzs(std::vector<std::string> tz_string_list) {
 
-  // Determining the current time:
+  // Retrieving the current time, then rounding it down to the 
+  // nearest second in order to simplify the output:
   // This code was based on
   // https://en.cppreference.com/w/cpp/chrono/system_clock/now.html .
-  std::chrono::time_point<std::chrono::system_clock> current_time =
-      std::chrono::system_clock::now();
-  // The following line was based on https://en.cppreference.com/w/cpp/chrono/time_point.html
-  auto unix_time_ns = current_time.time_since_epoch();
-  // The following line was based on https://en.cppreference.com/w/cpp/chrono/time_point/time_since_epoch
+  // I found that adding std::chrono::seconds as my duration argument
+  // helped ensure that the times printed by this function would
+  // indeed show seconds as integers rather than floats.
+  // std::chrono::time_point<std::chrono::system_clock>
+  std::chrono::time_point<std::chrono::system_clock, 
+  std::chrono::seconds> current_time =
+  std::chrono::floor<std::chrono::seconds>(
+  std::chrono::system_clock::now());
+
+  // For debugging:
+  // std::cout << "Current UTC time: " << std::chrono::system_clock::now() << "\n";
+  //std::cout << ty
+
+  // The following line was based on
+  // https://en.cppreference.com/w/cpp/chrono/time_point.html and
+  // https://en.cppreference.com/w/cpp/chrono/time_point/time_since_epoch .
   auto unix_time_s = std::chrono::duration_cast<std::chrono::seconds>(
-    unix_time_ns).count();
-  std::cout << current_time.time_since_epoch() << "\n";
-  std::cout << unix_time_s << "\n";
+    current_time.time_since_epoch()).count();
+  std::cout << "Unix Time: " <<  unix_time_s << "\n";
   // Printing out all time zone data:
   for (int i = 0; i < tz_string_list.size(); i++) {
-    std::cout << tz_string_list[i] << ": "
-              << get_tz_time(tz_string_list[i], current_time) << "\n";
+    std::string tz_name_str = tz_string_list[i];
+    std::string tz_time_str = get_tz_time(tz_name_str, current_time);
+    // To make daytime and nighttime hours easier to distinguish, 
+    // we'll color times from 8:00:00 to 19:59:59 green,
+    // and all othe rtimes magenta. We'll do this by (1) retrieving
+    // the hours component of the timestamp in integer form;
+    // (2) checking whether this integer is less than 8 or 
+    // greater than/equal to 20; and then specifying the ANSI escape
+    // code that corresponds to the desired color.
+    // (These escape codes were retrieved from
+    // https://en.wikipedia.org/wiki/ANSI_escape_code#Colors ).
+    int tz_time_hours = std::stoi(tz_time_str.substr(0, 2));
+    std::string time_color = "\033[32m";
+    if ((tz_time_hours < 8) || (tz_time_hours >= 20)) 
+      {time_color = "\033[35m";}
+    std::cout << tz_string_list[i] << ": " << time_color
+    << tz_time_str << "\033[0m" << "\n";
+              // We could also use "\033[K\n" to clear out any 
+              // text to the right of each line, but this should
+              // rarely be necessary--and the user could get the same
+              // result by just restarting the program.
   }
 }
 
@@ -75,25 +131,33 @@ int main() {
   std::cout << "\033[J";
   while (true)
   {
-
     // We'll want to next run the script as close to the top of
     // the following second as possible--which can be accomplished
-    // by first rounding up a system clock to get to the next second,
-    // then pausing the script until that second.
+    // by first rounding a system clock down to get to the 
+    // current second; adding exactly one second to it; and then
+    // pausing the script until that second.
     // This code was based on:
-    // https://en.cppreference.com/w/cpp/chrono/time_point/round ,
+    // https://en.cppreference.com/w/cpp/chrono/time_point/round ;
     // Bames53's response at
-    // https://stackoverflow.com/a/9747668/13097194 ,
+    // https://stackoverflow.com/a/9747668/13097194 ;
+    // https://cppreference.net/cpp/chrono/time_point.html; 
+    // https://en.cppreference.com/w/cpp/chrono/duration.html ;
     // and 
     // https://en.cppreference.com/w/cpp/thread/sleep_until.html .
+    // See also: https://stackoverflow.com/a/79802383/13097194
 
-    auto next_second = std::chrono::ceil<std::chrono::seconds>(
-    std::chrono::system_clock::now());
+    auto next_second = std::chrono::floor<std::chrono::seconds>(
+    std::chrono::system_clock::now()) + std::chrono::seconds(1);
     std::this_thread::sleep_until(next_second);
     // Returning to the top of the console:
     std::cout << "\033[1;1H";
     print_tzs(tz_string_list); 
-    //std::cout << "\033[J";
+  // At this point, we could also enter "\033[J" to clear out any 
+  // text below these lines, but this would only be necessary if 
+  // the terminal were resized--and the user could also achieve this
+  // same effect simply by restarting the program.
+      
+    // std::cout << "\033[J";
 
 
     // std::this_thread::sleep_until(
