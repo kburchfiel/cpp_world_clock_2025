adb31e9077e970310ccd033c1e36c67f2e522f09
Kenneth Burchfiel <kburchfiel@gmail.com>
2025-10-30 00:27:29 -0400

    Users can now specify their own time zones and corresponding labels

--- README.md
+++ README.md
@@ -1,11 +1,22 @@
-# Console World Clock (V2) [Work in progress]
+# Console World Clock (2025 Edition)
 
 By Kenneth Burchfiel
 
 Released under the MIT License
 
-![](Program_Screenshot.png)
+![](cwc_2025_screenshot.png)
 
-This simple C++ program displays the current time and date for a variety of time zones. I plan to update it soon to allow the user, via a configuration file, to specify which [time zones](https://en.wikipedia.org/wiki/List_of_tz_database_time_zones) to include.
+*Note: This program, like all of my other programs, was created *without* the use of generative AI tools.*
 
-This program, like all of my other programs, was created *without* the use of generative AI tools.
+This simple C++ command-line-interface program displays the current time and date for a list of user-specified time zones. Times will appear in green if they're later than or equal to 8:00:00 and earlier than 20:00:00; all other times will appear in cyan.
+
+The source code makes extensive use of [ANSI escape codes to control](https://en.wikipedia.org/wiki/ANSI_escape_code) the color and display of each time zone.
+
+
+## Setup notes
+
+1. Compile the program, either using CMake or directly via a compiler like G++. (I have only tested it on Linux, but it should work on Windows and Mac as well--perhaps with a few tweaks.)
+
+2. Update the tz_list.csv file as needed in order to specify which time zones you would like to display. Within this CSV file, the first row shows time zone database entries ([available here](https://en.wikipedia.org/wiki/List_of_tz_database_time_zones), and the second row shows the labels you'd like to assign to those entries. (These labels can include spaces, but they must not include commas.) World times will be displayed in the order that they're entered within this database. 
+
+    [This map](https://upload.wikimedia.org/wikipedia/commons/8/88/World_Time_Zones_Map.png) can help you identify which time zones you might want to add to your program.
--- cpp_world_clock.cpp
+++ cpp_world_clock.cpp
@@ -1,22 +1,6 @@
 /* C++ World Clock: 2025 Edition [Still a work in progress!]
 By Ken Burchfiel
 Released under the MIT License 
-
-// To-dos:
-1. Allow users to specify time zones via a config file that the
-script will read in
-3. Allow users to choose whether or not to show the Unix time entry
-
-(For your config file, you could place the time zone list at the 
-bottom and all other entries before it. Each entry could be preceded
-by an explanation, and you could hard-code the row numbers
-in your script for easier parsing. For instance, row 1 could be 
-"Show unix time", row 2 could be "true" or "false", 
-rows 3 and 4 could be an explanation/entry pair for some other
-configuration, row 5 could be "Time Zones", and rows 6 onward could
-be all the time zones. (The script would then loop from 5 to the end
-of all lines within the file to gather users' time zones.)
-
 */
 
 
@@ -26,31 +10,13 @@ of all lines within the file to gather users' time zones.)
 #include <vector>
 #include <thread>
 #include <string>
-
-// Initializing a list of time zone strings:
-// These can be found at:
-// https://en.wikipedia.org/wiki/List_of_tz_database_time_zones .
-std::vector<std::string> tz_string_list{
-    "US/Samoa",  
-    "Pacific/Honolulu",
-    "US/Pacific",
-    "US/Eastern",
-    "America/Puerto_Rico",
-    "UTC",        
-    "Europe/London",    
-    "Europe/Rome",     
-    "Asia/Jerusalem",
-    "Asia/Calcutta",
-    "Asia/Tokyo",
-    "Pacific/Guam",
-    "Australia/Sydney",
-    "Pacific/Kiritimati",};
-
+#include <fstream>
+#include <sstream>
 
 std::string get_tz_time(
-    std::string tz_string,
-    std::chrono::time_point<std::chrono::system_clock, 
-    std::chrono::seconds> current_time) {
+    const std::string& tz_string,
+    const std::chrono::time_point<std::chrono::system_clock, 
+    std::chrono::seconds>& current_time) {
   // Note: I found the actual type of system_clock::now() from
   // https://en.cppreference.com/w/cpp/chrono/system_clock/now .
 
@@ -72,7 +38,7 @@ std::string get_tz_time(
   //return tz_time;
 }
 
-void print_tzs(std::vector<std::string> tz_string_list) {
+void print_tzs(const std::vector<std::vector<std::string>>& tz_vec) {
 
   // Retrieving the current time, then rounding it down to the 
   // nearest second in order to simplify the output:
@@ -85,11 +51,7 @@ void print_tzs(std::vector<std::string> tz_string_list) {
   std::chrono::time_point<std::chrono::system_clock, 
   std::chrono::seconds> current_time =
   std::chrono::floor<std::chrono::seconds>(
-  std::chrono::system_clock::now());
-
-  // For debugging:
-  // std::cout << "Current UTC time: " << std::chrono::system_clock::now() << "\n";
-  //std::cout << ty
+  std::chrono::system_clock::now()); 
 
   // The following line was based on
   // https://en.cppreference.com/w/cpp/chrono/time_point.html and
@@ -99,10 +61,13 @@ void print_tzs(std::vector<std::string> tz_string_list) {
   std::cout << "Unix Time: " <<  unix_time_s << "\033[K\n";
     // \033[K clears out any lingering additional text to the right
     // of each line.
+
   // Printing out all time zone data:
-  for (int i = 0; i < tz_string_list.size(); i++) {
-    std::string tz_name_str = tz_string_list[i];
-    std::string tz_time_str = get_tz_time(tz_name_str, current_time);
+
+  for (const std::vector<std::string>& tz_vec_entry : tz_vec)
+  {
+
+    std::string tz_time_str = get_tz_time(tz_vec_entry[0], current_time);
     // To make daytime and nighttime hours easier to distinguish, 
     // we'll color times from 8:00:00 to 19:59:59 green,
     // and all othe rtimes magenta. We'll do this by (1) retrieving
@@ -116,9 +81,12 @@ void print_tzs(std::vector<std::string> tz_string_list) {
     std::string time_color = "\033[32m";
     if ((tz_time_hours < 8) || (tz_time_hours >= 20)) 
       {time_color = "\033[36m";}
-    std::cout << tz_string_list[i] << ": " << time_color
+    std::cout << tz_vec_entry[1] << ": " << time_color
     << tz_time_str << "\033[0m" << "\033[K\n";
   }
+    // For debugging: (This line allows you to see how quickly these
+    // lines get printed to the console.)
+  // std::cout << "Current UTC time: " << std::chrono::system_clock::now() << "\n";
 }
 
 int main() { 
@@ -126,6 +94,37 @@ int main() {
   // (For reference, see https://en.wikipedia.org/wiki/ANSI_escape_code)
     std::cout << "\033[1;1H";
     std::cout << "\033[J";
+
+
+std::vector<std::vector<std::string>> tz_vec;
+
+// Reading time zone database codes and user-specified titles
+// for each time zone from tz_list.csv:
+// Note: much of this input code was based on the examples
+// found at https://en.cppreference.com/w/cpp/string/basic_string/getline .
+std::ifstream tz_list_file {"../tz_list.csv"};
+// Retrieving each line within the .csv file:
+int line_count = 0;
+for (std::string tz_list_pair; std::getline(
+  tz_list_file, tz_list_pair);)
+  {// Discarding the header
+    if (line_count != 0)
+    {
+    std::istringstream tz_list_pair_inputs {tz_list_pair};
+    // Creating a vector that can store each entry within the file:
+    // (There's likely a way to skip this step and add items
+    // directly to tz_vec, but this approach will be more 
+    // flexible and can thus be repurposed for other programs
+    // that read in .csv data.
+    std::vector<std::string> tz_pair_vector {};
+    for (std::string item; std::getline(
+    tz_list_pair_inputs, item, ',');)
+      {tz_pair_vector.push_back(item);}
+    tz_vec.push_back(tz_pair_vector);
+    }
+    
+  line_count++;}
+  
   while (true)
   {
     // We'll want to next run the script as close to the top of
@@ -147,7 +146,7 @@ int main() {
     std::chrono::system_clock::now()) + std::chrono::seconds(1);
     std::this_thread::sleep_until(next_second);
     
-    print_tzs(tz_string_list); 
+    print_tzs(tz_vec); 
     // Clearing out the rest of the screen (which may be necessary
     // if the window had been resized):
     std::cout << "\033[J";
